b87230a68b3381fc227cc71f1420c251dc5fa090
Hariprasath2611 <hp5173690@gmail.com>
2026-02-04 15:45:38 +0530

    Updated project files

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_CoBrS_Q7.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DJhJW413.mjs';
-export { renderers } from '../renderers.mjs';
